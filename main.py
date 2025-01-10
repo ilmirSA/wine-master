b50537854dbd94e7eddf51c8bee39f74a8fcd010
ilmirSA <ilmir531@gmail.com>
2025-01-10 11:29:16 +0300

    - Добавил отступы в template.html - Разбил код на функции вынес сортировку в отдельную функцию - Вынес получение даты в отдельную функцию

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import collections
 import datetime
 from collections import OrderedDict
@@ -7,7 +8,25 @@ import pandas as pd
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def render_page(excel_result, year_now, year_opening):
+def sort_table(excel_result):
+    products = excel_result
+    grouped_wines = collections.defaultdict(list)
+
+    for wine in products:
+        grouped_wines[wine["Категория"]].append(wine)
+
+    sorted_wines = OrderedDict(grouped_wines)
+    sorted_wines.move_to_end('Напитки')
+    return sorted_wines
+
+
+def get_year(year_opening):
+    year_now = datetime.datetime.now().year
+    formatted_date = f"Уже {year_now - year_opening} года с вами"
+    return formatted_date
+
+
+def render_page(sorted_wines, year):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -15,16 +34,7 @@ def render_page(excel_result, year_now, year_opening):
 
     template = env.get_template('template.html')
 
-    products = excel_result
-    grouped_wines = collections.defaultdict(list)
-
-    for wine in products:
-        grouped_wines[wine["Категория"]].append(wine)
-
-    wine_sorting = OrderedDict(grouped_wines)
-    wine_sorting.move_to_end('Напитки')
-
-    page = template.render(wine_information=wine_sorting, years=f"Уже {year_now - year_opening} года с вами")
+    page = template.render(wine_information=sorted_wines, years=year)
 
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(page)
@@ -40,12 +50,20 @@ def read_excel(file_path):
 
 
 def main():
-    user_file = input("Введите путь до файла эксель: ")
-    file_path = user_file if user_file else "wine.xlsx"
-    year_now = datetime.datetime.now().year
+    parser = argparse.ArgumentParser(
+        description="Введите путь до файла эксель:"
+    )
+    parser.add_argument(
+        "--path",
+        help="Полный путь до таблицы эксель",
+        default="wine.xlsx"
+    )
+    args = parser.parse_args()
     year_opening = 1920
-    excel_result = read_excel(file_path)
-    render_page(excel_result, year_now, year_opening)
+    formatted_year = get_year(year_opening)
+    excel_result = read_excel(args.path)
+    sorted_wines = sort_table(excel_result)
+    render_page(sorted_wines, formatted_year)
 
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
